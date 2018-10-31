61bcd3794a5e480a5d3dd005f605db1108b2de94
Antonio Correia <antonio.alves.correia@gmail.com>
2018-10-31 02:12:17 -0300

    adicionando escalonamento de braço em c#

---
+++ Escalonamento de Braço - C#/escalonamento_braco/C_scan.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace escalonamento_braco
+{
+    public sealed class C_scan
+    {
+        private static readonly C_scan instance = new C_scan();
+
+        private List<int?> requisicoes;
+        private static int tamanho_disco;
+        private int i;
+        
+
+        static C_scan()
+        {
+        }
+
+        private C_scan()
+        {
+            
+            requisicoes = new List<int?>();
+            i = 1;
+            tamanho_disco = 40;
+            for (int i = 0; i < tamanho_disco; i++)
+            {
+                requisicoes.Add(null);
+            }
+        }
+        
+
+        public void Executa()
+        {
+            while (i <= tamanho_disco)//possuiRequisicao()
+            {
+                
+
+                if (requisicoes[i-1].HasValue)
+                {
+                    Console.WriteLine("Posição do disco: " + i.ToString() + " | Requisição: " + (i).ToString());
+                    resolverRequisicao(i-1);
+                }
+                else
+                {
+                    Console.WriteLine("Posição do disco: " + i.ToString() + " | Vazia");
+                }
+
+                i += 1;
+
+                //if (i == tamanho_disco)
+                //    i = 0;
+
+                Thread.Sleep(300);
+                
+            }
+
+            i = 1;
+        }
+
+        private void resolverRequisicao(int i)
+        {
+            requisicoes[i] = null;
+        }
+
+        private bool possuiRequisicao()
+        {
+            return requisicoes.Where(r => r.HasValue).Count() > 0;
+        }
+
+        public static C_scan Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void NovaRequisicao(int numero_requisicao)
+        {
+            //if (numero_requisicao >= tamanho_disco)
+            //    throw new Exception("número da requisição deve ser menor que tamanho do disco");
+
+            requisicoes[numero_requisicao-1] = numero_requisicao;
+        }
+    }
+}
---
+++ Escalonamento de Braço - C#/escalonamento_braco/Scan.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace escalonamento_braco
+{
+    public sealed class Scan
+    {
+        private static readonly Scan instance = new Scan();
+
+        private List<int?> requisicoes;
+        private static int tamanho_disco;
+        private int i;
+
+
+        static Scan()
+        {
+        }
+
+        private Scan()
+        {
+
+            requisicoes = new List<int?>();
+            i = 1;
+            tamanho_disco = 40;
+            for (int i = 0; i <= tamanho_disco; i++)
+            {
+                requisicoes.Add(null);
+            }
+        }
+
+
+        public void Executa()
+        {
+            if(i == 1)
+            {
+                while (i <= tamanho_disco) //possuiRequisicao()
+                {
+                    if (requisicoes[i-1].HasValue)
+                    {
+                        Console.WriteLine("Posição do disco: " + i.ToString() + " | Requisição: " + (i).ToString());
+                        resolverRequisicao(i-1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Posição do disco: " + i.ToString() + " | Vazia");
+                    }
+
+
+
+                    i += 1;
+
+                    Thread.Sleep(300);
+
+                }
+
+            }
+            else
+            {
+                i -= 1;
+
+                while (i > 0) //possuiRequisicao()
+                {
+                    if (requisicoes[i-1].HasValue)
+                    {
+                        Console.WriteLine("Posição do disco: " + i.ToString() + " | Requisição: " + (i).ToString());
+                        resolverRequisicao(i-1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Posição do disco: " + i.ToString() + " | Vazia");
+                    }
+
+                    i -= 1;
+
+                    Thread.Sleep(300);
+
+                }
+                
+            }
+            
+
+            
+        }
+
+        private void resolverRequisicao(int i)
+        {
+            requisicoes[i] = null;
+        }
+
+        private bool possuiRequisicao()
+        {
+            return requisicoes.Where(r => r.HasValue).Count() > 0;
+        }
+
+        public static Scan Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void NovaRequisicao(int numero_requisicao)
+        {
+            //if (numero_requisicao > tamanho_disco)
+            //    throw new Exception("número da requisição deve ser menor que tamanho do disco");
+
+            requisicoes[numero_requisicao-1] = numero_requisicao;
+        }
+    }
+}
